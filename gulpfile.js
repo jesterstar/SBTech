a81e3fd649191aeca5f0818dfd3bb1ecc0bbb167
sergey.glazov <sergey.glazov@kitsoft.kiev.ua>
2017-10-12 19:16:23 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp    = require('gulp');
+var sass    = require('gulp-sass');
+var	concat  = require('gulp-concat');
+var	cssnano = require('gulp-cssnano');
+var	rename  = require('gulp-rename');
+var gutil   = require('gulp-util');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config');
+
+gulp.task('webpack', function(callback) {
+    webpack(webpackConfig, function(err, stats) {
+        if(err) throw new gutil.PluginError('webpack', err);
+        gutil.log("[webpack]", stats.toString({
+        }));
+        callback();
+    });
+});
+
+gulp.task('sass', function(){
+    return gulp.src('assets/sass/main.+(scss|sass)')
+        .pipe(sass())
+        .pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('css-libs', ['sass'], function(){
+    return gulp.src('assets/css/main.css')
+        .pipe(concat('styles.min.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('build', ['webpack','css-libs']);
+
+
+//watcher SASS
+gulp.task('watch', ['css-libs', 'sass', 'webpack'], function(){
+    gulp.watch('assets/sass/**/*+(scss|sass)', ['sass']);
+    gulp.watch('assets/javascript/src/**/*.js', ['webpack']);
+});
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+'use strict';
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const env = process.env.NODE_ENV;
+
+const location = {
+	build: './assets/javascript/build',
+	home: './assets/javascript/src/pages/home'
+};
+
+// define plugins
+const plugins = (function(env) {
+	let plugins = [];
+	
+	if(env === 'production') {
+		plugins.push(new CleanWebpackPlugin([location.build]));
+	}
+	
+	return plugins;
+})(env);
+
+module.exports = {
+	resolve: {
+		extensions: ['.js', '.json'],
+		modules: ['node_modules' ]
+	},
+	entry: {
+		home: location.home
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, location.build)
+	},
+	plugins: plugins,
+	module: {
+		rules: [
+      {
+          enforce: "pre",
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "eslint-loader",
+      },
+			{
+				test: /\.(js)$/,
+                loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.handlebars$/,
+				loader: "handlebars-loader"
+			}
+		]
+	}
+};
